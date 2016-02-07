c19342d9ecba850c4557811316e296c51727b3c0
Tretiakoff <c.tretiakoff@gmail.com>
2016-02-07 19:47:22 +0100

    btn

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ window.onload=function(){
             document.getElementById('pokemon_image').style.backgroundImage='url("http://img.pokemondb.net/artwork/' +
                 name.toLowerCase() + '.jpg")';
             document.getElementById('error').innerHTML='';
+            myForm.elements['number'].value = '';
         }
         else {
             var number = 0;
@@ -63,7 +64,50 @@ window.onload=function(){
                 document.getElementById('pokemon_image').style.backgroundImage='url("http://img.pokemondb.net/artwork/'
                     + name.toLowerCase() + '.jpg")';
                 document.getElementById('error').innerHTML='';
+                myForm.elements['number'].value = '';
             }
         }
     }
+
+
+    document.getElementById('btn1').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '1';
+    };
+    document.getElementById('btn2').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '2';
+    };
+    document.getElementById('btn3').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '3';
+    };
+    document.getElementById('btn4').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '4';
+    };
+    document.getElementById('btn5').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '5';
+    };
+    document.getElementById('btn6').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '6';
+    };
+    document.getElementById('btn7').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '7';
+    };
+    document.getElementById('btn8').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '8';
+    };
+    document.getElementById('btn9').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '9';
+    };
+    document.getElementById('btn0').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value + '0';
+    };
+
+    document.getElementById('delete').onclick = function () {
+        myForm.elements['number'].value = myForm.elements['number'].value.slice(0, -1);
+    };
+    document.getElementById('send').onclick = function () {
+        myForm.elements['submit'].click();
+        myForm.elements['number'].value = '';
+    };
+
+
 };
